a5a5a32e5e0de7a1ca05bdd43fb1ef496eeed289
CIT195 <122820747+Holmes1776@users.noreply.github.com>
2023-03-09 23:16:27 -0500

    Add project files.

---
+++ LambdaExpressions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LambdaExpressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter in a number");
+            double first = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter in a second number");
+            double second = double.Parse(Console.ReadLine());
+            var sum = (double first, double second) => first + second;
+            var product = (double first, double second) => first * second;
+            var bigger = (double first, double second) =>
+            {
+                if (first > second)
+                {
+                    return first;
+                }
+                else
+                {
+                    return second;
+                }
+            };
+            Console.WriteLine("Here is the sum of the numbers");
+            Console.WriteLine(sum(first, second));
+            Console.WriteLine("Here is the product of the numbers");
+            Console.WriteLine(product(first, second));
+            Console.WriteLine("Here is the larger of the 2 numbers");
+            Console.WriteLine(bigger(first, second));
+            
+        }
+    }
+}
